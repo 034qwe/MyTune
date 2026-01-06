a970b7c8c3e730d0dff367168258980616d7c48f
bohdan <bnestercuk91@gmail.com>
2026-01-06 16:48:36 +0200

    google url

--- MyTunes/MyTunes/urls.py
+++ MyTunes/MyTunes/urls.py
@@ -7,6 +7,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from coolAuth.views import google_auth
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +16,7 @@ urlpatterns = [
     path('',include('community.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('google-auth/', google_auth, name = 'google-auth' ),
     
 ] 
 
