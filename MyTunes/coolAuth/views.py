e0b8611ff85b254c889ed2719ca5b0fecb8c5905
bohdan <bnestercuk91@gmail.com>
2026-02-02 22:47:39 +0200

    is admin permission

--- MyTunes/coolAuth/views.py
+++ MyTunes/coolAuth/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import logging
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 logger = logging.getLogger(__name__)
 
@@ -30,7 +30,7 @@ class MakeAdminAPIView(APIView):
                 status=status.HTTP_200_OK
                 )
 
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAdminUser,)
 
 @api_view(["POST"])
 def google_auth(request):
